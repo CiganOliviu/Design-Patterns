f700b009e820ab7ade6276a7db0c94ea11c2dff1
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2021-05-07 21:01:24 +0300

    Create Flyweight pattern

---
+++ Structural-Patterns/FlyWeightPattern/src/com/BebiSoft/F16.java
@@ -0,0 +1,13 @@
+package com.BebiSoft;
+
+public class F16 {
+
+    private final String name = "F16";
+    private final int personnel = 2;
+    private final String dimensions = "15m long 3m wide";
+    private final String wingSpan = "33 feet";
+
+    public double PredictRemainedFuel(int litersOfFuel, int currentSpeed) {
+        return 1;
+    }
+}
---
+++ Structural-Patterns/FlyWeightPattern/src/com/BebiSoft/Main.java
@@ -0,0 +1,11 @@
+package com.BebiSoft;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        F16 f16 = new F16();
+
+        f16.PredictRemainedFuel(300, 900);
+    }
+}
